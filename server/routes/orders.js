93d0bf8bb71960ca2da1c73e227aebc0e22f0615
Manoj kumar <manojkumarpalakuri@gmail.com>
2026-01-28 01:12:50 +0530

    chore: remove debug logs (final production cleanup)

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -6,8 +6,6 @@ const { protect, admin } = require('../middleware/auth');
 const sendEmail = require('../utils/sendEmail');
 const Notification = require('../models/Notification');
 
-console.log('>>> Orders Route Loaded');
-
 // Helper for title casing names
 const titleCase = (str) => {
     if (!str) return '';
